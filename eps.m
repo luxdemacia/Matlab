ac6effd6100fbd4bec46f41accd3d61050e565e0
luxdemacia <elhadjiboubacartraore@gmail.com>
2025-02-20 17:28:17 +0100

    Eps introduction

---
+++ eps.m
@@ -0,0 +1,6 @@
+x = 0.1 + 0.2;
+disp(x == 0.3)  % Vérifie si 0.1 + 0.2 est exactement égal à 0.3
+
+%Si vou ne voyez pas l'erreur (car ce que fait par défaut matlab) faite  un disp:
+x=0.1+0.2;
+disp(x-0.3)
